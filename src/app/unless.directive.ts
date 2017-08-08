79f6f57d1bb368c28bd97869f061641123090212
Rei Lin <temisrei@gmail.com>
2017-08-08 23:06:32 +0800

    自訂 結構型指令 Structural Directives

--- src/app/unless.directive.ts
+++ src/app/unless.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appUnless]'
 })
 export class UnlessDirective {
+  @Input() set appUnless(condition: boolean) {
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vcRef.clear();
+    }
+  }
 
-  constructor() { }
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
 
 }
